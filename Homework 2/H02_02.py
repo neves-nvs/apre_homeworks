d349f3bba6264b433e4c8e941fc33bde91580afb
Diogo Ferreira <57763474+neves-nvs@users.noreply.github.com>
2021-10-24 23:48:38 +0100

    question 5 progress, updated breast.w.arff

--- Homework 2/H02_02.py
+++ Homework 2/H02_02.py
@@ -1,15 +1,35 @@
+#!/usr/local/bin/python3
+
+import pandas as pd
+import numpy as np
+
 from scipy.io import arff
 
-from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import SelectKBest
+
+from sklearn.feature_extraction.text import CountVectorizer
 
 # Extract Data
 data = pd.DataFrame( arff.loadarff( "breast.w.arff" )[0] )
 # Elements array
-X = data.drop(columns=data.columns[-1]).to_numpy().astype(int)
-# Results array
+X = data.drop(columns=data.columns[-1])#.to_numpy().astype(int)
+# Results array binarized
 Y = data[data.columns[-1]].replace(b'benign', 0).replace(b'malignant', 1)
 
+def q5_1():
+    for k_val in [1, 3, 5, 9]:
+        KBest = SelectKBest(mutual_info_classif, k=k_val).fit_transform(X, Y)
+        
+        clf = DecisionTreeClassifier()
+        clf.fit(KBest, Y)
+    
 
-clf = tree.DecisionTreeClassifier()
-clf.fit(X, Y)
+def q5_2():
+    for k_val in [1, 3, 5, 9]:
 
+        clf = DecisionTreeClassifier(max_depth=k_val)
+
+q5_1()
