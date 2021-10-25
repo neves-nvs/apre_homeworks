1730e0c85026d4ccffc483b23e1b77a48de9d4d8
Diogo Ferreira <57763474+neves-nvs@users.noreply.github.com>
2021-10-25 12:24:42 +0100

    almost finished programming part

--- Homework 2/H02_02.py
+++ Homework 2/H02_02.py
@@ -15,6 +15,8 @@ from sklearn.feature_selection import SelectKBest
 
 from sklearn.feature_extraction.text import CountVectorizer
 
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
 GROUPN = 39
 
 # Extract Data
@@ -25,23 +27,61 @@ X = data.drop(columns=data.columns[-1]).to_numpy().astype(int)
 Y = data[data.columns[-1]].replace(b'benign', 0).replace(b'malignant', 1)
 
 def q5_1():
+     
+    train_errors_list, test_errors_list = list(), list()  
+        
     for k_val in [1, 3, 5, 9]:
+
         KBest = SelectKBest(mutual_info_classif, k=k_val).fit_transform(X, Y)
         
         X_train, X_test, Y_train, Y_test = train_test_split(KBest, Y, random_state=GROUPN)
-
+        
         clf = DecisionTreeClassifier()
 
         clf.fit(X_train, Y_train)
 
-        #predict
+        Y_train_predict = clf.predict(X_train)
+        Y_test_predict = clf.predict(X_test)
 
+        train_error = mean_absolute_error(Y_train, Y_train_predict)
+        test_error = mean_absolute_error(Y_test, Y_test_predict)
+
+        train_errors_list.append(train_error)
+        test_errors_list.append(test_error)
+
+        err = mean_squared_error(train_errors_list, test_errors_list)    
+        print(f"Mean square error: {err} (k = {k_val})")
+
+    print(train_errors_list, test_errors_list)
         
     
 
 def q5_2():
-    for k_val in [1, 3, 5, 9]:
 
+    train_errors_list, test_errors_list = list(), list()  
+
+    for k_val in [1, 3, 5, 9]:
+        
+        KBest = SelectKBest(mutual_info_classif, k=k_val).fit_transform(X, Y)
+        
+        X_train, X_test, Y_train, Y_test = train_test_split(KBest, Y, random_state=GROUPN)
+        
         clf = DecisionTreeClassifier(max_depth=k_val)
 
-q5_1()
+        clf.fit(X_train, Y_train)
+
+        Y_train_predict = clf.predict(X_train)
+        Y_test_predict = clf.predict(X_test)
+
+        train_error = mean_absolute_error(Y_train, Y_train_predict)
+        test_error = mean_absolute_error(Y_test, Y_test_predict)
+
+        train_errors_list.append(train_error)
+        test_errors_list.append(test_error)
+
+        err = mean_squared_error(train_errors_list, test_errors_list)    
+        print(f"Mean square error: {err} (k = {k_val})")
+
+    print(train_errors_list, test_errors_list)
+
+q5_2()
