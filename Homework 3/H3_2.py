b391811bdb9fc5551b9ac53efa6e7e3306cd6c43
Baby Yoda das Neves <dnevesf01@gmail.com>
2021-11-03 21:26:33 +0000

    programming basically made

--- Homework 3/H3_2.py
+++ Homework 3/H3_2.py
@@ -1,12 +1,24 @@
 
-
-import matplotlib.pyplot as plt
-
 import pandas as pd
+
+import matplotlib as plt
+import numpy as np
+
 from scipy.io import arff
 
+from sklearn.neural_network import MLPClassifier
 
+from sklearn.datasets import make_classification
+
+from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import confusion_matrix
+
+# LEARNING_RATE = 0.001
 GROUPN = 95
+
+knn = KFold(n_splits=5, random_state=GROUPN, shuffle=True)
 
 
 def quest2():
@@ -16,18 +28,40 @@ def quest2():
     X = D_breast.drop(columns=D_breast.columns[-1]).to_numpy().astype(int)
     # Results array binarized
     Y = D_breast[D_breast.columns[-1]].replace(b'benign', 0).replace(b'malignant', 1)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=GROUPN)
+
+    clf = MLPClassifier(random_state=GROUPN)
+    clf.fit(X_train, Y_train)
+
+    Y_test_predict = clf.predict(X_test)
+
+    conf_matrix = confusion_matrix(Y_test, Y_test_predict)
+    
+    print(Y_test_predict)
+    print(conf_matrix)
+
+
+
 
 def quest3():
     # Extract Data
     D_kin = pd.DataFrame( arff.loadarff( "kin8nm.arff" )[0] )
     # Elements array
-    X = D_kin.drop(columns=D_kin.columns[-1]).to_numpy().astype(int)
+    X = D_kin.drop(columns=D_kin.columns[-1]).to_numpy()
     # Results array binarized
-    # Y = D_kin[D_kin.columns[-1]].replace(b'benign', 0).replace(b'malignant', 1)
+    Y = D_kin[D_kin.columns[-1]].to_numpy()
 
-    print(D_kin)
-    print("--------------------")
-    print(X)
+    print(Y)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=GROUPN)
 
-quest2()
+    clf = MLPClassifier(random_state=GROUPN)
+    clf.fit(X_train, Y_train)
+
+    # Y_test_predict = clf.predict(X_test)
+
+    # residuals = np.subtract(Y_test, Y_test_predict)
+    # print(residuals, Y_test_predict)
+    # plt.scatter(residuals, Y_test_predict)
+
+# quest2()
 quest3()
