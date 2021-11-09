4842222df604a6813dd9159f8c1212c7b58047aa
Baby Yoda das Neves <dnevesf01@gmail.com>
2021-11-09 22:42:23 +0000

    change something i think

--- Homework 4/H4_2.py
+++ Homework 4/H4_2.py
@@ -9,37 +9,70 @@ from scipy.io import arff
 from sklearn.cluster import KMeans
 
 from sklearn.metrics import silhouette_score
-# from sklearn.metrics import #ECR
+
+from sklearn.feature_selection import mutual_info_classif
+
+from sklearn.feature_selection import SelectKBest
 
 SEED = 95
 
+# Extract Data
+D_breast = pd.DataFrame( arff.loadarff( "breast.w.arff" )[0] )
+# Elements array
+X = D_breast.drop(columns=D_breast.columns[-1]).to_numpy().astype(int)
+# Results array binarized
+Y = D_breast[D_breast.columns[-1]].replace(b'benign', 0).replace(b'malignant', 1)
+
 def quest4():
-    # Extract Data
-    D_breast = pd.DataFrame( arff.loadarff( "breast.w.arff" )[0] )
-    # Elements array
-    X = D_breast.drop(columns=D_breast.columns[-1]).to_numpy().astype(int)
-    # Results array binarized
-    Y = D_breast[D_breast.columns[-1]].replace(b'benign', 0).replace(b'malignant', 1)
 
-    kmeans_2 = KMeans(n_clusters=2, random_state=SEED)
-    kmeans_2 = kmeans_2.fit(X)
-    Y2_pred = kmeans_2.predict(X)
+    def Cluster(k, Y_pred):
+        cluster = np.where(Y_pred == k) [0]
+        return cluster
 
-    sil_score2 = silhouette_score(X, Y2_pred, random_state=SEED)
-    print(sil_score2)
-    
-    
-    kmeans_3 = KMeans(n_clusters=2, random_state=SEED)
-    kmeans_3 = kmeans_3.fit(X)
+    def ECR(k, Y, Y_pred):
+        soma = 0
 
+        for i in range(k):
+            cluster = Cluster(i, Y_pred)
+            total = [0]*k
 
+            for ind in cluster:
+                total[Y[ind]] += 1
+            phi = max(total)
+
+            soma += (len(cluster) - phi)
+
+        return (1/k) * soma
+
+    for k in [2, 3]:
+        # Train model
+        kmeans = KMeans(n_clusters=k, random_state=SEED).fit(X)
+        Y_pred = kmeans.predict(X)
+
+        print()
+        print(f"------------ K = {k} ------------")
+        # Error classification rate (external measure)
+        ecr = ECR(k, Y, Y_pred)
+        print(f"       ECR = {ecr}")
+        # Silhouette coefficient (internal measure)
+        sil_score = silhouette_score(X, Y_pred, random_state=SEED)
+        print(f"silhouette = {sil_score}")
 
 def quest5():
-    pass
+    kmeans = KMeans(n_clusters=3, random_state=SEED)
+    kmeans = kmeans.fit(X)
+    Y_pred = kmeans.predict(X)
 
-def quest6():
-    pass
+    best_feat = SelectKBest(mutual_info_classif, k=2).fit_transform(X, Y)
+    print(best_feat)
+
+    # pprint(dir(best_feat))
+    # print(best_feat)
+
+    # for feature in range(2):
+    #     plt.scatter(feature, Y2_pred)
+
+    plt.savefig("graph5")
 
 quest4()
-quest5()
-quest6()
+# quest5()
