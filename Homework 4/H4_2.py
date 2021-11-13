3a0e84ab02b1ea27a8960bc2bd945ce3f507fded
Baby Yoda das Neves <dnevesf01@gmail.com>
2021-11-13 19:08:21 +0000

    finished graph

--- Homework 4/H4_2.py
+++ Homework 4/H4_2.py
@@ -1,23 +1,16 @@
 
 import pandas as pd
-
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import matplotlib.lines as mlines
-
 import numpy as np
-
 from scipy.io import arff
-
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.feature_selection import mutual_info_classif
 from sklearn.feature_selection import SelectKBest
 
-import pprint
-
-
-SEED = 95
+SEED = 39
 
 # Extract Data
 D_breast = pd.DataFrame( arff.loadarff( "breast.w.arff" )[0] )
@@ -25,8 +18,6 @@ D_breast = pd.DataFrame( arff.loadarff( "breast.w.arff" )[0] )
 X = D_breast.drop(columns=D_breast.columns[-1]).to_numpy().astype(int)
 # Results array binarized
 Y = D_breast[D_breast.columns[-1]].replace(b'benign', 0).replace(b'malignant', 1)
-
-
 
 def quest4():
 
@@ -41,10 +32,8 @@ def quest4():
             cluster = Cluster(i, Y_pred)
             total = [0]*k
 
-            for ind in cluster:
-                total[Y[ind]] += 1
+            for ind in cluster: total[Y[ind]] += 1
             phi = max(total)
-
             soma += (len(cluster) - phi)
 
         return (1/k) * soma
@@ -54,45 +43,36 @@ def quest4():
         kmeans = KMeans(n_clusters=k, random_state=SEED).fit(X)
         Y_pred = kmeans.predict(X)
 
-        print()
-        print(f"------------ K = {k} ------------")
+        print(f"\n------------ K = {k} ------------")
         # Error classification rate (external measure)
         ecr = ECR(k, Y, Y_pred)
-        print(f"       ECR = {ecr}")
+        print(f"ECR = {ecr}")
         # Silhouette coefficient (internal measure)
         sil_score = silhouette_score(X, Y_pred, random_state=SEED)
-        print(f"silhouette = {sil_score}")
+        print(f"Silhouette = {sil_score}")
 
 
 def quest5():
-    kmeans = KMeans(n_clusters=3, random_state=0)
-    kmeans = kmeans.fit(X)
-    Y_pred = kmeans.predict(X)
+    kmeans = KMeans(n_clusters=3, random_state=SEED)
+    Y_pred = kmeans.fit_predict(X)
 
-    # Get elements with only their 2 most meaningful features
     Kbest = SelectKBest(mutual_info_classif, k=2)
     Kbest = Kbest.fit_transform(X, Y)
-    # Separate KBest by their output class
 
     def marker(i):
         if Y_pred[i] == 0: return "o"
         elif Y_pred[i] == 1: return "^"
         elif Y_pred[i] == 2: return "D"
-        else: print("Bad classification")
 
     def color(i):
         if Y[i] == 0: return "green"
         elif Y[i] == 1: return "red"
-        else: print("Data missing")
    
     for i, point in enumerate(Kbest):
         plt.scatter(point[0], point[1], alpha=0.2, marker=marker(i), color=color(i))
 
     cluster_centers = kmeans.cluster_centers_
-    
-    print(cluster_centers)
-    
-
+    for c in cluster_centers: plt.scatter(c[1], c[2], marker="x", color="black")
 
     plt.title("K = 3 Clustering Solution based on top 2 features")
     plt.xlabel("Cell_Size_Uniformity")
@@ -100,17 +80,14 @@ def quest5():
 
     green = mpatches.Patch(color='green', label='Malignant')
     red = mpatches.Patch(color='red', label='Benign')
-
-    cl_1 = mlines.Line2D([], [], marker='D', color="black", linestyle='None', markersize=6, label='Cluster 1')
-    cl_2 = mlines.Line2D([], [], marker='o', color="black", linestyle='None', markersize=8, label='Cluster 2')
+    cl_1 = mlines.Line2D([], [], marker='o', color="black", linestyle='None', markersize=8, label='Cluster 1')
+    cl_2 = mlines.Line2D([], [], marker='D', color="black", linestyle='None', markersize=6, label='Cluster 2')
     cl_3 = mlines.Line2D([], [], marker='^', color="black", linestyle='None', markersize=7, label='Cluster 3')
+    cl_c = mlines.Line2D([], [], marker='x', color="black", linestyle='None', markersize=7, label='Centroids')
 
-    plt.legend(handles=[green, red, cl_1, cl_2, cl_3])
-    # plt.legend("hey", title="Clusters", loc=(10.5, 10.5))
-    # plt.legend("oh", title="CLass", loc=(10.05, 2))
+    plt.legend(handles=[green, red, cl_1, cl_2, cl_3, cl_c])
 
     plt.savefig("graph5")
-
 
 quest4()
 quest5()
